42f6b28f4e602e37db861eea2ca632bd21f1257a
CristhianY <cristhian.pachari@upeu.edu.pe>
2022-05-12 17:56:34 +0000

    kato

--- EstRepetivivas/Ejercicio.java
+++ EstRepetivivas/Ejercicio.java
@@ -1,30 +1,48 @@
 package EstRepetivivas;
 
+import java.util.Scanner;
+
 /**
- * Ejercicio
+ * EjerciciosR
  */
 public class Ejercicio {
 
-    public static void imprimirWhileN1_10() {
-        int numInit=1;
-        while (numInit>=20) {
+    private static int i;
+
+    public static void imprimirWhileN1_20() {
+        int numInit=21;
+        while (numInit<=20) {
             System.out.println(numInit);
             numInit=numInit+1;
-            
         }
-        
     }
-    public static void imprimirWhileN1_20() {
-        int numInit=1;
+    public static void imprimirDowhileN1_20() {
+        int numInit=21;
         do {
             System.out.println(numInit);
+            numInit++;
         } while (numInit<=20);
+    }
+
+    public static void imprimirForN1_20() {
+         for (int numInit = 1; numInit <= 20; numInit++) {
+             System.out.println(numInit);
+         }
+    }
+
+    public static void nombreInverso() {
+        Scanner lt=new Scanner(System.in);
+        System.out.println("Ingrese su nombre:");
+        String nombre=lt.next();
+        for (int indiex = nombre.length()-1; i >=0; i--) {
+            System.out.println(nombre.charAt(i));
+        }
         
     }
 
     public static void main(String[] args) {
-        System.out.println("Imprime While:"); imprimirWhileN1_10();
-        System.out.println("Imprime Do While:"); imprimirWhileN1_10();
+       System.out.println("Imprime While:");  imprimirWhileN1_20();
+       System.out.println("Imprime Do while:"); imprimirDowhileN1_20();
+       System.out.println("Imprime for:"); imprimirForN1_20();
     }
-
 }
