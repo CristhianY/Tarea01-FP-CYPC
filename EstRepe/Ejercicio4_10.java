af11610b1882ae652eefe8901bc9cf2ce965015f
CristhianY <cristhian.pachari@upeu.edu.pe>
2022-05-19 18:02:31 +0000

    kato

---
+++ EstRepe/Ejercicio4_10.java
@@ -0,0 +1,27 @@
+package EstRepe;
+
+import java.util.Scanner;
+
+public class Ejercicio4_10 {
+    static Scanner leerT=new Scanner(System.in);
+
+    public static long double factorialN(int numero) {
+        long double resultado=1;
+        if (numero>1) {
+            for (int i = 1; i <= numero; i++) {
+                resultado=resultado*i;
+                
+            }
+            }
+        return resultado;
+    }
+    public static void main(String[] args) {
+        System.out.println("Ingrese un numero:");
+        int num=leerT.nextInt();
+        System.out.println(factorialN(num));
+        for (int i = 1; i <= num; i++) {
+            System.out.println("Factorial de " + i + "es:" +factorialN(i));
+        }
+    }
+    
+}
